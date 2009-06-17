a9dd4b00cc4f76967a844824fb197c4cf335cd94
Robby Russell <robby@planetargon.com>
2009-06-16 23:38:35 -0700

    Adding a few more specs for Engine to show different examples.

--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 module LessEngineSpecHelper
   def lessify(string)
-    return Less::Engine.new(string)
+    return Less::Engine.new(string).to_css(:desc)
   end
 end
 
@@ -11,15 +11,23 @@ describe Less::Engine do
 
   describe "to_css" do
     it "should return p {} for p {}" do
-      lessify('p {}').to_css(:desc).should == 'p { }'
+      lessify('p {}').should == 'p { }'
     end
 
-    it "should return p {} for p {}" do
-      lessify("@brand_color: #4D926F; \n#header { color: @brand_color; }").to_css(:desc).should == "#header { color: #4D926F; }"
+    it "should return css with variable usage" do
+      lessify("@brand_color: #4D926F;\n #header { color: @brand_color; }").should == "#header { color: #4D926F; }"
     end
 
-    it "should return p {} for p {}" do
-      lessify("#header { \n  color: red;\n  a {\n    font-weight: bold;\n    text-decoration: none;\n  }\n}").to_css(:desc).should == "#header a { font-weight: bold; text-decoration: none; }\n#header { color: red; }"
+    it "should return css as shown on home page" do
+      lessify("#header { \n  color: red;\n  a {\n    font-weight: bold;\n    text-decoration: none;\n  }\n}").should == "#header a { font-weight: bold; text-decoration: none; }\n#header { color: red; }"
+    end
+
+    it "should handle :hover" do
+      lessify("a:hover {\n color: red; }").should == "a:hover { color: red; }"
+    end
+
+    it "should handle instances of double-quotes" do
+      lessify("blockquote:before, blockquote:after, q:before, q:after {\n content: \"\"; }").should == "blockquote:before, blockquote:after, q:before, q:after { content: ''; }"
     end
   end
 end
