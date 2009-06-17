8714406e7934eec0b3566a20cfd738fcbc72b18a
Robby Russell <robby@planetargon.com>
2009-06-16 22:36:25 -0700

    Using unshift for spec-compatibility

--- lib/less.rb
+++ lib/less.rb
@@ -1,20 +1,22 @@
+$:.unshift File.dirname(__FILE__)
+
 require 'cgi'
 
 require 'less/command'
 require 'less/engine'
 require 'less/tree'
 
-module Less    
+module Less
   MixedUnitsError = Class.new(Exception)
   PathError = Class.new(Exception)
-  
+
   def self.version
     File.read( File.join( File.dirname(__FILE__), '..', 'VERSION') )
   end
 end
 
 class Hash
-  # Convert a hash into a Tree  
+  # Convert a hash into a Tree
   def to_tree
     Less::Tree.new self
   end
