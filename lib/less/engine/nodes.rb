9e3f9ba0f81a6ee63985fa8dcfeb7374a6e88306
cloudhead <alexis@cloudhead.net>
2009-07-19 16:44:09 -0400

    init

--- lib/less/engine/nodes.rb
+++ lib/less/engine/nodes.rb
@@ -2,6 +2,7 @@ $:.unshift File.dirname(__FILE__)
 
 require 'engine/nodes/entity'
 require 'engine/nodes/function'
+require 'engine/nodes/ruleset'
 require 'engine/nodes/element'
 require 'engine/nodes/property'
 require 'engine/nodes/literal'
---
+++ lib/less/engine/nodes/ruleset.rb
@@ -0,0 +1,12 @@
+module Less
+  module Node
+    class Ruleset < Array
+      def initialize elements = []
+        super elements
+      end
+      
+      def to_css
+      end
+    end
+  end
+end
