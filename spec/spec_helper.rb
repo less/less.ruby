42299989505a4ba77d163f7f57bd3365503a52a2
cloudhead <self@cloudhead.net>
2009-10-11 18:35:24 -0400

    fuck LOAD_PATH

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'spec'
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+$:.unshift File.dirname(__FILE__)
 
 require 'less'
 
