076445b65408d9925bcdb75364831a81ffb15a3f
cloudhead <self@cloudhead.net>
2010-04-19 21:24:03 -0400

    import support

--- lib/less/engine.rb
+++ lib/less/engine.rb
@@ -17,6 +17,24 @@ module Less
       @options = options
       @parser = Johnson::Runtime.new.load(
         "#{File.dirname(__FILE__)}/engine/less.js")
+
+      @parser.setImporter(lambda do |file, paths, &callback|
+        paths ||= []
+        paths.push @path
+
+        path = paths.each do |p|
+          p = "#{File.expand_path(p)}/#{file}"
+          break p if File.exists?(p)
+        end
+
+        @parser.parser().parse(File.read(path)) do |err, tree|
+          if err
+            raise Less::ParserError, err.message
+          else
+            callback.call(tree)
+          end
+        end
+      end)
     end
 
     def parse &blk
